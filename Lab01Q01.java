1d33e06ad71d03bbe622e7904580d0a4fdca0e50
Navinu <IT24101505@my.sliit.lk>
2025-07-30 12:48:08 +0530

    Add Lab01Q01 Java program

---
+++ Lab01Q01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Lab01Q01 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Enter your First Name: ");
+        String firstName = scanner.nextLine();
+
+        
+        System.out.print("Enter your Last Name: ");
+        String lastName = scanner.nextLine();
+
+        
+        System.out.println("Welcome to the Second Year " + firstName + " " + lastName);
+
+        
+        scanner.close();
+    }
+}
